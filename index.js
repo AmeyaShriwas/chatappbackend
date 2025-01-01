3312bb591fc592579e70d4f9d0a9746b8225f04d
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2025-01-01 23:32:53 +0530

    cors

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: '*'}))
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 // Middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
