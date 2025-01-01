d96a82062d08b16062ebc62a6f572e590ec82701
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2025-01-01 18:33:21 +0530

    initial

---
+++ Models/UserModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    number: { type: String, required: true },
+    otp: { type: String, required: false },
+    isVerified: { type: Boolean, default: false },
+    isDelete: { type: Boolean, default: false },
+    profilePicture: { type: String, default: "" },
+    lastSeen: { type: Date, default: Date.now },
+    isOnline: { type: Boolean, default: false },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    resetToken: { type: String, required: false },
+    resetTokenExpiry: { type: Date, required: false },
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    status: { type: String, default: "" },
+  },
+  { timestamps: true } // Automatically adds createdAt and updatedAt
+);
+
+module.exports = mongoose.model("User", userSchema);
